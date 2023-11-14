7018ba716200c650bb0bbbf87badd274a2e95f1b
Joshua <Joshua.soto@live.com>
2023-11-14 17:04:46 -0600

    fixed syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATA IF EXISTS company_db;
+DROP DATA company_db;
 
 CREATE DATABASE company_db;
 
@@ -6,18 +6,18 @@ USE company_db;
 
 CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30)
+name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
-FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
